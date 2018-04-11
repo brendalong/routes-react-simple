2c1aa51621e10f65c8c9b59007e3d0197a69eceb
Brenda Long <brenda@studio6thave.com>
2018-04-11 06:35:14 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Routing Example
+
+Based on: https://medium.freecodecamp.org/beginners-guide-to-react-router-4-8959ceb3ad58
---
+++ src/components/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './App.css';
+import Hello from './Hello';
+import About from './About';
+import Books from './Books';
+import Home from './Home';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch,
+  Redirect
+} from 'react-router-dom'
+
+//Be sure to npm install react-router-dom
+//react-router-dom includes three packages: 
+//react-router - core package
+//react-router-dom - web
+//react-router-native - mobile
+
+// import route Components here
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <div className="App">
+        <div className="container">
+                {/* Link replaces the default <a href=""></a> HTML tag. It accepts a to="" prop that points to the URL location we want to go to. */}
+                  <ul>
+                    <li><Link to="/hello">Hello</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/books">Books</Link></li>
+                  </ul>
+                  <hr/>
+        {/* Routes will go here */}
+        {/* The component that will be created via Route will automatically be passed the followingprop objects: match, location, history */}
+        {/* Route component renders a page if the current URL location matches the path prop specified in it. It also accepts component, render and children props. */}
+        {/* All paths have /, so we need to include exact if we just want one item*/}       
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/hello" component={Hello} />
+                <Route path="/books" component={Books} />
+        {/* Check out Switch as well */ }
+        </div>
+      </div>
+      </Router>
+    );
+  }
+}
+export default App;
